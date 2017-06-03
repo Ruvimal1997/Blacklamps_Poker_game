9cb88d4f5730a8a8f1f260a0ed3f3e5f9a8e008d
Ruvimal1997 <ruvimal@gmail.com>
2017-06-03 21:35:30 +0530

    21:04 Ruvimal - Main Menu

--- src/Foker/Game/Controllers/Login.java
+++ src/Foker/Game/Controllers/Login.java
@@ -20,7 +20,6 @@ public class Login extends Application {
         scene.getStylesheets().add("/Resources/CSS.css");
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Foker Game");
-        String s;
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
