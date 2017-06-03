37c7f42293d1b88fa0106bd75c2b9cf6c5a224ef
Vimukthi391 <vimukthi391@gmail.com>
2017-06-04 00:25:14 +0530

    00:24 AM Vimukthi Join

--- src/Foker/Game/Controllers/JoinGameController.java
+++ src/Foker/Game/Controllers/JoinGameController.java
@@ -4,7 +4,7 @@ import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import javafx.scene.control.ComboBox;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
@@ -18,9 +18,16 @@ public class JoinGameController implements Initializable
 
     @FXML
     private Button Btn_Play;
+
+    @FXML
+    private ComboBox<?> Cmb_SelectPlayer;
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
+        //Cmb_SelectPlayer.getItems().removeAll(Cmb_SelectPlayer.getItems());
+       // Cmb_SelectPlayer.getItems().addAll("Option A", "Option B", "Option C");
+        //Cmb_SelectPlayer.getSelectionModel().select("Option B");
 
     }
 }
