ac2e96f928c2839abffff9b4df67340448a4ab9d
Ruvimal1997 <ruvimal@gmail.com>
2017-06-03 23:45:33 +0530

    23:41 Ruvimal - Join game controller

--- src/Foker/Game/Controllers/JoinGameController.java
+++ src/Foker/Game/Controllers/JoinGameController.java
@@ -1,7 +1,28 @@
 package Foker.Game.Controllers;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 /**
  * Created by Ruvimal Senanayake on 03/06/2017.
  */
-public class JoinGameController {
+public class JoinGameController implements Initializable
+{
+    @FXML
+    private Pane Pane;
+
+    @FXML
+    private Button Btn_Play;
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
 }
+
+
