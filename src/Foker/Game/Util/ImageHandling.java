abc430b488ae95f1036ed9c16cfb8983c6be4d36
Ruvimal1997 <ruvimal@gmail.com>
2017-06-04 11:07:42 +0530

    10:53 Ruvimal - Images class

---
+++ src/Foker/Game/Util/ImageHandling.java
@@ -0,0 +1,67 @@
+package Foker.Game.Util;
+
+/**
+ * Created by Ruvimal Senanayake on 04/06/2017.
+ */
+public class ImageHandling {
+
+    //All Faces  of the Suit Heart
+    private final static String HA = "/Images/Hearts/hearts_ace.png";
+    private final static String H2 = "/Images/Hearts/hearts_2.png";
+    private final static String H3 = "/Images/Hearts/hearts_3.png";
+    private final static String H4 = "/Images/Hearts/hearts_4.png";
+    private final static String H5 = "/Images/Hearts/hearts_5.png";
+    private final static String H6 = "/Images/Hearts/hearts_6.png";
+    private final static String H7 = "/Images/Hearts/hearts_7.png";
+    private final static String H8 = "/Images/Hearts/hearts_8.png";
+    private final static String H9 = "/Images/Hearts/hearts_9.png";
+    private final static String H10 = "/Images/Hearts/hearts_10.png";
+    private final static String Hj = "/Images/Hearts/hearts_jack.png";
+    private final static String Hq = "/Images/Hearts/hearts_queen.png";
+    private final static String Hk = "/Images/Hearts/hearts_king.png";
+
+    //All Faces  of the Suit Diamond
+    private final static String DA = "/Images/Diamonds/diamonds_ace.png";
+    private final static String D2 = "/Images/Diamonds/diamonds_2.png";
+    private final static String D3 = "/Images/Diamonds/diamonds_3.png";
+    private final static String D4 = "/Images/Diamonds/diamonds_4.png";
+    private final static String D5 = "/Images/Diamonds/diamonds_5.png";
+    private final static String D6 = "/Images/Diamonds/diamonds_6.png";
+    private final static String D7 = "/Images/Diamonds/diamonds_7.png";
+    private final static String D8 = "/Images/Diamonds/diamonds_8.png";
+    private final static String D9 = "/Images/Diamonds/diamonds_9.png";
+    private final static String D10 = "/Images/Diamonds/diamonds_10.png";
+    private final static String Dj = "/Images/Diamonds/diamonds_jack.png";
+    private final static String Dq = "/Images/Diamonds/diamonds_queen.png";
+    private final static String Dk = "/Images/Diamonds/diamonds_king.png";
+
+    //All Faces  of the Suit Club
+    private final static String CA = "/Images/Clubs/clubs_ace.png";
+    private final static String C2 = "/Images/Clubs/clubs_2.png";
+    private final static String C3 = "/Images/Clubs/clubs_3.png";
+    private final static String C4 = "/Images/Clubs/clubs_4.png";
+    private final static String C5 = "/Images/Clubs/clubs_5.png";
+    private final static String C6 = "/Images/Clubs/clubs_6.png";
+    private final static String C7 = "/Images/Clubs/clubs_7.png";
+    private final static String C8 = "/Images/Clubs/clubs_8.png";
+    private final static String C9 = "/Images/Clubs/clubs_9.png";
+    private final static String C10 = "/Images/Clubs/clubs_10.png";
+    private final static String Cj = "/Images/Clubs/clubs_jack.png";
+    private final static String Cq = "/Images/Clubs/clubs_queen.png";
+    private final static String Ck = "/Images/Clubs/clubs_king.png";
+
+    //All Faces  of the Suit Spades
+    private final static String SA = "/Images/Spades/spades_ace.pngg";
+    private final static String S2 = "/Images/Spades/spades_2.png";
+    private final static String S3 = "/Images/Spades/spades_3.png";
+    private final static String S4 = "/Images/Spades/spades_4.png";
+    private final static String S5 = "/Images/Spades/spades_5.png";
+    private final static String S6 = "/Images/Spades/spades_6.png";
+    private final static String S7 = "/Images/Spades/spades_7.png";
+    private final static String S8 = "/Images/Spades/spades_8.png";
+    private final static String S9 = "/Images/Spades/spades_9.png";
+    private final static String S10 = "/Images/Spades/spades_10.png";
+    private final static String Sj = "/Images/Spades/spades_jack.png";
+    private final static String Sq = "/Images/Spades/spades_queen.png";
+    private final static String Sk = "/Images/Spades/spades_king.png";
+}
